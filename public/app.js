ccd93e00eef07ceff1b3f9794de1e66783c8793b
rachida mabrour <rachida.mabrour7@gmail.com>
2022-01-26 02:22:50 +0800

    first commit

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
-const socket = io('http://localhost:3000');
-const form = document.querySelector('form');
-const messages = document.querySelector('.messages');
-const contacts = document.querySelector('.contacts');
+const socket = io("http://localhost:3000");
+const form = document.querySelector("form");
+const messages = document.querySelector(".messages");
+const contacts = document.querySelector(".contacts");
 
 let user;
 let newMessage;
@@ -10,211 +10,166 @@ let newUser;
 let newReceiver = {};
 let receiver;
 
-
-form.addEventListener('submit', processForm);
+form.addEventListener("submit", processForm);
 
 let message_send = "message-send";
-let message_rcv = "message-rcv"; 
+let message_rcv = "message-rcv";
 let status_connected = "status-connected";
 let status_disconnected = "status-disconnected";
 
+function processForm(e) {
+  if (e.preventDefault) e.preventDefault();
 
+  let message = form.elements["message"].value;
 
-function processForm(e){
+  newMessage = {
+    message: message,
+    sender: user,
+    receiver: newReceiver,
+  };
 
-    if(e.preventDefault)
-        e.preventDefault();
+  socket.emit("newMessage", newMessage);
 
-    let message = form.elements['message'].value;
+  let p = document.createElement("p");
+  p.innerHTML = message;
+  let div = document.createElement("div");
 
-    newMessage = {
-        message : message,
-        sender : user,
-        receiver : newReceiver
+  div.className = "message " + message_send;
+
+  div.append(p);
+  messages.append(div);
+}
+
+socket.on("user", (use) => {
+  user = use;
+});
+
+socket.on("friends", (friends) => {
+  friends.forEach((friend) => {
+    let contactlink = document.createElement("a");
+    contactlink.className = "contactlink";
+    contactlink.href = "#";
+    let div = document.createElement("div");
+    div.className = "contact";
+    let h1 = document.createElement("h1");
+    h1.className = "name";
+    newUser = friend[0];
+    h1.innerHTML = newUser.username;
+    contactlink.addEventListener("click", () => {
+      socket.emit("usernametoget", h1.innerHTML);
+      socket.emit("getmessages", { user1: user, user2: newReceiver });
+    });
+
+    let statusdiv = document.createElement("div");
+    statusdiv.className = "status " + status_disconnected;
+
+    div.append(h1);
+    div.append(statusdiv);
+    contactlink.append(div);
+    contacts.append(contactlink);
+  });
+});
+
+socket.on("receiver", (receiver) => {
+  newReceiver.id = receiver._id;
+  newReceiver.email = receiver.email;
+  newReceiver.username = receiver.username;
+  newReceiver.password = receiver.password;
+});
+
+socket.on("message", (message) => {
+  console.log("message recieved");
+
+  console.log(message.receiver);
+  console.log(user);
+  if (message.receiver.username == user.username) {
+    appendIntoMessages(message);
+  }
+});
+
+socket.on("userstatus", (userstatus) => {
+  let children = contacts.childNodes;
+  let array = [...children];
+  let divcontacts;
+
+  array.forEach((element) => {
+    if (element.className === "contactlink") {
+      if (element.innerText === userstatus.username) {
+        let divstatus = element.querySelector(".status");
+        divstatus.className = "status " + userstatus.status;
+        console.log(divstatus);
+      }
     }
+  });
+});
 
-    socket.emit('newMessage', newMessage);
+socket.on("user disconnected", (userstatus) => {
+  let children = contacts.childNodes;
+  let array = [...children];
+  let divcontacts;
 
-    let p = document.createElement('p');
-    p.innerHTML = message;
-    let div = document.createElement('div');
+  array.forEach((element) => {
+    if (element.className === "contactlink") {
+      if (element.innerText === userstatus.username) {
+        let divstatus = element.querySelector(".status");
+        divstatus.className = "status " + userstatus.status;
+        console.log(divstatus);
+      }
+    }
+  });
+});
 
-    
-    div.className = "message "+message_send;
+socket.on("msgs", (msgs) => {
+  refreshMessages();
+
+  msgs.sort((a, b) => new Date(a.date) - new Date(b.date));
+  msgs.forEach((message) => {
+    let div = document.createElement("div");
+
+    let p = document.createElement("p");
+    if (message.sent === false && message.rcv === true)
+      div.className = "message " + message_rcv;
+    else if (message.sent === true && message.rcv === false)
+      div.className = "message " + message_send;
+
+    p.innerHTML = message.message;
 
     div.append(p);
     messages.append(div);
-}
-
-
-socket.on('user', use => {
-    user = use;
-})
-
-socket.on('friends', friends => {
-
-
-    friends.forEach(friend => {
-    let contactlink = document.createElement('a')
-    contactlink.className = 'contactlink';
-    contactlink.href = "#"
-    let div = document.createElement('div');
-    div.className = 'contact';
-    let h1 = document.createElement('h1');
-    h1.className = 'name';
-    newUser = friend[0];
-    h1.innerHTML = newUser.username;
-    contactlink.addEventListener('click', () => {
-        
-        socket.emit('usernametoget', h1.innerHTML);
-        socket.emit('getmessages', {user1 : user, user2 : newReceiver});
-        
-    })
-
-   
-    let statusdiv = document.createElement('div');
-    statusdiv.className = 'status ' + status_disconnected;
-
-    div.append(h1);
-    div.append(statusdiv);    
-    contactlink.append(div);
-    contacts.append(contactlink);
-    });
-
-    
-});
-    
-
-socket.on('receiver', receiver => {
-    newReceiver.id = receiver._id;
-    newReceiver.email = receiver.email;
-    newReceiver.username = receiver.username;
-    newReceiver.password = receiver.password;
+  });
 });
 
+socket.on("currentConnections", (currentConnections) => {
+  let children = contacts.childNodes;
+  let array = [...children];
+  let divcontacts;
 
-socket.on('message', message => {
-    console.log('message recieved')
-
-    console.log(message.receiver)
-    console.log(user)
-    if(message.receiver.username == user.username){
-        appendIntoMessages(message);
+  array.forEach((element) => {
+    if (element.className === "contactlink") {
+      currentConnections.forEach((connection) => {
+        if (element.innerText === connection.username) {
+          let divstatus = element.querySelector(".status");
+          divstatus.className = "status " + connection.status;
+          console.log(divstatus);
+        }
+      });
     }
-        
-})
+  });
+});
 
-socket.on('userstatus', userstatus => {
-
-    let children = contacts.childNodes;
-    let array = [...children];
-    let divcontacts;
-
-    array.forEach(element => {
-        
-        if(element.className === 'contactlink'){
-            if(element.innerText === userstatus.username){
-                let divstatus = element.querySelector('.status')
-                divstatus.className = 'status '+userstatus.status;
-                console.log(divstatus);
-            }
-        }
-    })  
-
-
-
-})
-
-socket.on('user disconnected', userstatus => {
-
-    let children = contacts.childNodes;
-    let array = [...children];
-    let divcontacts;
-
-    array.forEach(element => {
-        
-        if(element.className === 'contactlink'){
-            if(element.innerText === userstatus.username){
-                let divstatus = element.querySelector('.status')
-                divstatus.className = 'status '+userstatus.status;
-                console.log(divstatus);
-            }
-        }
-    })  
-
-})
-
-
-
-
-socket.on('msgs', msgs => {
-
-    refreshMessages();
-
-    msgs.sort((a,b) => new Date(a.date) - new Date(b.date));
-    msgs.forEach(message => {
-        let div = document.createElement('div');
-        
-        let p = document.createElement('p');
-        if(message.sent === false && message.rcv === true)
-            div.className = 'message '+ message_rcv;
-        else if(message.sent === true && message.rcv === false)
-            div.className = 'message '+ message_send;
-        
-    
-        p.innerHTML = message.message;
-
-        div.append(p);
-        messages.append(div)
-    })
-
-})
-
-
-
-socket.on('currentConnections', currentConnections => {
-    let children = contacts.childNodes;
-    let array = [...children];
-    let divcontacts;
-
-    array.forEach(element => {
-        if(element.className === 'contactlink'){
-
-            currentConnections.forEach(connection => {
-                
-                    if(element.innerText === connection.username){
-                        let divstatus = element.querySelector('.status')
-                        divstatus.className = 'status '+connection.status;
-                        console.log(divstatus);
-                    }
-
-            })
-        }
-
-       
-        
-    })  
-})
-
-function refreshMessages(){
-    messages.innerHTML = '';
+function refreshMessages() {
+  messages.innerHTML = "";
 }
 
-function appendIntoMessages(message){
-    let div = document.createElement('div');
-        
-        let p = document.createElement('p');
-        
-        div.className = 'message '+ message_rcv;
-        
+function appendIntoMessages(message) {
+  let div = document.createElement("div");
 
-    
-        p.innerHTML = message.message;
+  let p = document.createElement("p");
 
+  div.className = "message " + message_rcv;
 
-        div.append(p);
-        messages.append(div)
+  p.innerHTML = message.message;
+
+  div.append(p);
+  messages.append(div);
 }
-
-
-
